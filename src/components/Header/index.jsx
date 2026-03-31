7737e08a128db6e6ba919d5b261034125a24dc2b
bruna <brunaduarte2307@gmail.com>
2026-03-30 22:12:55 -0300

    react

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Header() {
+    return (
+        <header>
+            <h1> exemplo de react</h1>
+        </header>
+
+    )
+}
+
+export default Header 
---
+++ src/components/List/index.jsx
@@ -0,0 +1,24 @@
+import "./style.css"
+
+function List() {
+    return (
+        <section>
+            <h2 id="exemplo-h2" >Seção de Listas</h2>
+            <h3>Lista não ordenada</h3>
+            <ul id="ul" >
+                <li>Batata</li>
+                <li>Pão</li>
+                <li>Arroz</li>
+            </ul>
+            <h3>Lista Ordenada</h3>
+            <ol>
+                <li>Monster High</li>
+                <li>Musga</li>
+                <li>Ioutube</li>
+            </ol>
+
+        </section>
+    )
+
+}
+export default List 
